2e2612cf7baef5a12a949329b16dc15a0f7094cd
sop26 <sopie.soppy@gmail.com>
2023-06-10 23:51:44 -0700

    semi working stuff :)

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 class Textbook {
-  constructor(name, author, description, cost, isbn, accID, img) {
+  constructor(name, author, description, cost, isbn, username, img) {
     this.name = name;
     this.author = author;
     this.desc = description;
     this.cost = cost;
     this.isbn = isbn;
-    this.accID = accID;
+    this.username = username;
     this.img = img;
-    this.unavail = false;
+    // this.sold = false;
   }
   getName() {
     return this.name;
@@ -24,75 +24,110 @@ class Textbook {
   getISBN() {
     return this.isbn;
   }
-  getaccID() {
-    return this.accID;
+  getUsername() {
+    return this.username;
   }
-  unavail() {
+  getImg() {
+    return this.img;
+  }
+  sold() {
     this.sold = true;
-  }
-  }
-
-class Account {
-  constructor(name, location, contact, id) {
-    this.name = name;
-    this.location = location;
-    this.contact = contact;
-    this.id = id;
-    this.starred = [];
-  }
-  getContact(){
-    
-  }
-  addStar(){
-    
   }
 }
 
+class Account {
+  constructor(name, username, location, contact, postings) {
+    this.name = name;
+    this.username = username;
+    this.location = location;
+    this.contact = contact;
+    this.postings = postings;
+  }
+  getContact() {
+    return this.contact;
+  }
+  getPostings() {
+    return this.postings;
+  }
+  addPosting(book) {
+    this.postings.push(book);
+  }
+  // addStar() {}
+}
 
 var textbooks = [
-    new Textbook(
-    'The Earth and Its Peoples : A Global History: Sixth Edition', 'Bullet',
-    'AP Edition, quality: Fair', '$68', '978-1285436838', '01984roaiufdljksdf'
-    ),
-    new Textbook(
-    'hello', 'Bullet',
-    'AP Edition, quality: Fair', '$68', '978-1285436838'
-    )
+  new Textbook(
+      'World History Volume 2: From 1400', 'Ann Kordas, et al.',
+      'Condition: like new', '$40', '9781711471433,9781711471440,9781951693626',
+      'amndazhang',
+      'https://cdnagesdb.com/images/booksimages/D6FDE14D981339F5D8736717C08D3DD3.webp'),
+  new Textbook(
+      'The Earth and Its Peoples : A Global History: Sixth Edition', 'Bullet',
+      'AP Edition, quality: Fair', '$68', '978-1285436838', 'sopieli',
+      'https://m.media-amazon.com/images/I/516mKQySQWL._SX373_BO1,204,203,200_.jpg'),
+  new Textbook(
+      'Calculus 9th ed.', 'Larson, Edward', 'Condition: Great', '$347.95',
+      '978-0547167022', 'amndazhang',
+      'https://m.media-amazon.com/images/I/41HlPvXdNML._SX399_BO1,204,203,200_.jpg'),
+  new Textbook(
+      'Biology Of Flowering Plants', 'Skene', 'Condition: Bad', '$56.41',
+      '9788171412051', 'valval',
+      'https://isbncoverdb.com/images/book-cover-biology-of-flowering-plants.webp')
+];
 
+// window.localStorage.setItem('textbook', JSON.stringify(textbooks));
+// textbooks = JSON.parse(window.localStorage.getItem('textbook'));
+
+var accounts = [
+  new Account(
+      'Prog frogs', 'progfrogs8681', 'San Francisco Bay Area, CA',
+      'frog@frogs.com', []),
+  new Account(
+      'Amanda', 'amndazhang', 'San Francisco Bay Area, CA', 'amanda@frogs.com',
+      []),
+  new Account(
+      'Sophie', 'sopieli', 'San Francisco Bay Area, CA', 'sopie@frogs.com', []),
+  new Account(
+      'Valerie', 'valval', 'San Francisco Bay Area, CA', 'valerie@frogs.com',
+      []),
+  new Account(
+      'Prog rocks', 'rocks', 'San Francisco Bay Area, CA', 'prog@frogs.com', [])
 ];
 
 
 function search_item() {
-  let input = document.getElementById('searchbar').value
-  input = input.toLowerCase();
-  let x =  document.getElementsByClassName('item');
-//   let count = 0;
-  for(let i = 0; i < textbooks.length; i++) {
-    if (!textbooks[i].getName().toLowerCase().includes(input)) {
-        x[0].style.display = 'none';
-    } 
-    else {
-      x[1].children[0].innerText = textbooks[i].getName();
-      x[1].children[1].innerText = textbooks[i].getAuth();
-    //   x[1].children[2].innerText = textbooks[i].getDesc();
-      x[1].children[3].innerText = textbooks[i].getCost();
-    // displayImage("img/park.png", 3, 3);
-
-    //   x[1].children[4].innerText = textbooks[i].getISBN();
-    //   x[1].children[5].innerText = textbooks[i].getaccID();
-      x[1].children[6].innerText = textbooks[i].sold();
-    //   count++;
-    }
+  let input = document.getElementById('searchbar').value.toLowerCase();
+  let x = document.getElementsByClassName(
+      'item');  // list of txtbooks from index file
+  var textbooks = JSON.parse(window.localStorage.getItem('textbook'));
+  for (let i = 0; i < textbooks.length; i++) {
+    textbooks[i] = new Textbook(
+        textbooks[i].name, textbooks[i].author, textbooks[i].desc,
+        textbooks[i].cost, textbooks[i].isbn, textbooks[i].username,
+        textbooks[i].img)
   }
+  console.log(textbooks);
+  for (let i = 0; i < textbooks.length; i++) {
+    if (!textbooks[i].getName().toLowerCase().includes(input)) {
+      x[i].style.display = 'none';
+    }
+    if (textbooks[i].getName().toLowerCase().includes(input) ||
+        textbooks[i].getAuth().toLowerCase().includes(input) ||
+        textbooks[i].getCost().toLowerCase().includes(input)) {
+      x[i].style.display = 'list-item';
+      document.getElementById('image').src = textbooks[i].getImg();
+      x[i].children[1].innerText = textbooks[i].getName();
+      x[i].children[2].innerText = textbooks[i].getAuth();
+      x[i].children[3].innerText = textbooks[i].getCost();
+    }
+    console.log(textbooks);
+  }
+  //   x[i].style.display = 'none';
 }
 
-// function displayImage(src, width, height) {
-//     var img = document.createElement("img");
-//     img.src = src;
-//     img.width = width;
-//     img.height = height;
-//     document.body.appendChild(img);
-//    }
+// function sync() {}
+
+
 
 // function search_item() {
 //   let input = document.getElementById('searchbar').value
@@ -108,6 +143,67 @@ function search_item() {
 //   }
 // }
 
-// function post_item(){
-  // textbooks.
-// }
+window.onload =
+    function() {  // ensures the page is loaded before functions are executed.
+  document.getElementById('retrieveButton').onclick = retrieveRecords
+
+}
+
+function retrieveRecords() {  // retrieves items in the localStorage
+  console.log('retrieve records');
+  var records = window.localStorage.getItem(book);
+}
+
+function prep_load() {
+  if (window.localStorage.getItem('textbook') == null) {
+    window.localStorage.setItem('textbook', JSON.stringify(textbooks));
+  }
+} function post_item() {
+  const book = new Textbook(
+      document.getElementById('nameInput').value,
+      document.getElementById('authorInput').value,
+      document.getElementById('descriptionInput').value,
+      document.getElementById('costInput').value,
+      document.getElementById('isbnInput').value,
+      document.getElementById('accIdInput').value,
+      document.getElementById('imageInput').value);
+  var textbook = JSON.parse(window.localStorage.getItem('textbook'));
+  textbook.push(book);
+  window.localStorage.setItem('textbook', JSON.stringify(textbook));
+
+
+  // window.localStorage.setItem(new Textbook(
+  //     document.getElementById('nameInput').value,
+  //     document.getElementById('authorInput').value,
+  //     document.getElementById('descriptionInput').value,
+  //     document.getElementById('costInput').value,
+  //     document.getElementById('isbnInput').value,
+  //     document.getElementById('accIdInput').value,
+  //     document.getElementById('imageInput').value))
+  console.log(textbooks);
+  // document.getElementById('footer').innerText = "BALAJSFLSDFHWOIGRH";
+  // username.addPosting(book);
+
+
+  //   let x = document.getElementsByClassName('myitem');
+  //   document.getElementById('image').src = img;
+  //   x[0].children[1].innerText = name;
+  //   x[0].children[2].innerText = author;
+  //   x[0].children[3].innerText = cost;
+  //   x[0].style.display = 'list-item';
+}
+
+function show_item(id) {
+  bookTitle = document.getElementById(id).innerHTML;
+  console.log(textbooks.length);
+  for (i = 0; i < textbooks.length; i++) {
+    if (bookTitle == textbooks[i].getName()) {
+      id = i;
+    }
+  }
+  return bookTitle;
+}
+
+function load_item() {
+  document.getElementById('bookTitle').innerHTML = show_item('item1');
+}
